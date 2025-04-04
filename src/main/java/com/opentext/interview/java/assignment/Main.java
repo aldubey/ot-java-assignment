b322f5b9b620da74bf7c2a86479d54b11b678e9a
Alok Dubey <alok.dwivedi0609@gmail.com>
2025-04-04 12:45:14 +0530

    Fixed Task Ordering issue and code cleanup

--- src/main/java/com/opentext/interview/java/assignment/Main.java
+++ src/main/java/com/opentext/interview/java/assignment/Main.java
@@ -1,7 +1,6 @@
 package com.opentext.interview.java.assignment;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -14,7 +13,9 @@ public class Main {
 
     AtomicInteger taskRank = new AtomicInteger(0);
     static Map<String, Long> taskOrderMap = new ConcurrentHashMap<>();
-    static ConcurrentLinkedQueue taskQueue =new ConcurrentLinkedQueue<>();
+    static ConcurrentLinkedQueue taskQueue = new ConcurrentLinkedQueue<>();
+    private final ReentrantLock taskQueueLock = new ReentrantLock();
+    private final Condition taskWaitCon = taskQueueLock.newCondition();
 
 
     /**
@@ -68,11 +69,69 @@ public class Main {
         static ExecutorService executorService = Executors.newFixedThreadPool(5);
         private final ReentrantLock reentrantLock = new ReentrantLock();
         private final Condition condition = reentrantLock.newCondition();
+
         private ConcurrentHashMap<TaskGroup, UUID> runningTaskGroups = new ConcurrentHashMap<>();
 
         @Override
         public <T> Future<T> submitTask(Task<T> task) {
             Future<T> future = null;
+            try {
+                taskQueueLock.lock();
+                taskQueue.add(task);
+            } finally {
+                taskQueueLock.unlock();
+            }
+            handleSameTaskGroups(task);
+            future = handleTaskSubmission(task);
+
+            return future;
+        }
+
+        /**
+         * Submit to executor service and remove taskgroup from CHM which is done
+         *
+         * @param task
+         * @param <T>
+         * @return
+         */
+        private <T> Future<T> handleTaskSubmission(Task<T> task) {
+            Future<T> future;
+            future = executorService.submit(() -> {
+                while (true) {
+                    try {
+                        taskQueueLock.lock();
+                        if (taskQueue.peek() == null || task.equals(taskQueue.peek())) {
+                            break;
+                        }
+                        taskWaitCon.await();
+                    } finally {
+                        taskQueueLock.unlock();
+                    }
+
+                }
+                try {
+                    return task.taskAction().call();
+                } finally {
+                    reentrantLock.lock();
+                    try {
+                        runningTaskGroups.remove(task.taskGroup());
+                        //    System.out.println("Task with id " + task.taskUUID() + "and group id " + task.taskGroup().groupUUID() + "is done and remove from list");
+                        condition.signalAll();
+                    } finally {
+                        reentrantLock.unlock();
+                    }
+                }
+            });
+            return future;
+        }
+
+        /**
+         * same taskGrop tasks to wait untill the running task completes
+         *
+         * @param task
+         * @param <T>
+         */
+        private <T> void handleSameTaskGroups(Task<T> task) {
             try {
                 reentrantLock.lock();
                 while (runningTaskGroups.containsKey(task.taskGroup())) {
@@ -88,24 +147,8 @@ public class Main {
                 reentrantLock.unlock();
             }
             runningTaskGroups.put(task.taskGroup(), task.taskGroup().groupUUID());
-            future = executorService.submit(() -> {
-                try {
-                    //  System.out.println("@@@@ Task for input: " + input + " started by " + curThread + " rank:" +taskRank.incrementAndGet());
-                    return task.taskAction().call();
-                } finally {
-                    reentrantLock.lock();
-                    try {
-                        runningTaskGroups.remove(task.taskGroup());
-                        //    System.out.println("Task with id " + task.taskUUID() + "and group id " + task.taskGroup().groupUUID() + "is done and remove from list");
-                        condition.signalAll();
-                    } finally {
-                        reentrantLock.unlock();
-                    }
-                }
-            });
-
-            return future;
         }
+
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
@@ -140,7 +183,8 @@ public class Main {
             // System.out.println("Future Result : " + result.get());
             result.get();
         }
-
+        System.out.println("\n\n");
+        System.out.println("----------Order of Task Start in nanoTime--------- \n");
         taskOrderMap.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getValue)).forEach(e -> {
             System.out.println("@@@@ Task for input: " + e.getKey() + " start time: " + e.getValue());
         });
@@ -149,19 +193,34 @@ public class Main {
     }
 
     private String getLongRunTaskResult(String input) throws InterruptedException {
-        System.out.println("Task "+input+"Start Time: "+ Instant.now());
+        taskQueuePoll();
+        System.out.println("Task " + input + " Start Time: " + Instant.now());
         taskOrderMap.put(input, System.nanoTime());
-        Thread.sleep(10000);
-        System.out.println("Task "+input+"End Time: "+ Instant.now());
+        Thread.sleep(2000);
+        System.out.println("Task " + input + " End Time: " + Instant.now());
         return Optional.ofNullable(input).map(String::toUpperCase).orElse(input);
     }
 
     private String getShortRunTaskResult(String input) throws InterruptedException {
-        System.out.println("Task "+input+"Start Time: "+ Instant.now());
+        taskQueuePoll();
+        System.out.println("Task " + input + " Start Time: " + Instant.now());
         taskOrderMap.put(input, System.nanoTime());
         //Thread.sleep(4000);
-        System.out.println("Task "+input+"End Time: "+ Instant.now());
+        System.out.println("Task " + input + " End Time: " + Instant.now());
         return Optional.ofNullable(input).map(String::toUpperCase).orElse(input);
+    }
+
+    /**
+     * removes task from taskQueue so that next task can start
+     */
+    private void taskQueuePoll() {
+        taskQueueLock.lock();
+        try {
+            taskQueue.poll();
+            taskWaitCon.signalAll();
+        } finally {
+            taskQueueLock.unlock();
+        }
     }
 
 }
